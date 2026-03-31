1de275d04458cfca47c27087b487649fa982b3f2
YOKESH T <tyokesh793@gmail.com>
2026-03-31 22:49:08 +0530

    disk_audit.sh

--- disk_audit.sh
+++ disk_audit.sh
@@ -1,38 +1,7 @@
 #!/bin/bash
-# Script 3: Disk and Permission Auditor
-# Author: YOKESH T
-
-
-DIRS=("/etc" "/var/log" "/home" "/usr/bin" "/tmp")
-
-
-echo "Directory Audit Report"
-echo "----------------------"
-
-
-for DIR in "${DIRS[@]}"; do
-    if [ -d "$DIR" ]; then
-        PERMS=$(ls -ld $DIR | awk '{print $1, $3, $4}')
-        SIZE=$(du -sh $DIR 2>/dev/null | cut -f1)
-        echo "$DIR => Permissions: $PERMS | Size: $SIZE"
-    else
-        echo "$DIR does not exist on this system"
-    fi
-done
-
-
-echo "----------------------------------------"
-echo "Software Config Directory Check"
-echo "----------------------------------------"
-
-
-# Change this according to your chosen software
-CONFIG_DIR="/etc/vlc"
-
-
-if [ -d "$CONFIG_DIR" ]; then
-    CONFIG_INFO=$(ls -ld $CONFIG_DIR | awk '{print $1, $3, $4}')
-    echo "$CONFIG_DIR exists => Permissions: $CONFIG_INFO"
-else
-    echo "$CONFIG_DIR does NOT exist"
-fi
+echo "Answer the following questions:"
+read -p "1. Open-source tool you use daily: " TOOL read -p "2. Meaning of freedom (one word): " FREEDOM read -p "3. Something you want to build: " BUILD
+DATE=$(date)
+OUTPUT="manifesto.txt"
+echo "On $DATE, I believe in the power of open source." > $OUTPUT echo "Using tools like $TOOL, I experience $FREEDOM." >> $OUTPUT
+echo "In the future, I aim to build $BUILD and share it freely with the world." >> $OUTPUT echo "Manifesto saved in $OUTPUT" cat $OUTPUT
